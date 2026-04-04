a9a8b1c74b0c1662c9ec9faccae98fe3d296d284
Anupam Mondal <anupammondal621@gmail.com>
2026-04-04 22:27:12 +0200

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header";
 import Breadcrumb from "@/components/Breadcrumb";
 
 export const metadata = {
-  title: "My Shop",
+  title: "BongoMithai",
   description: "My online shop",
 };
 
