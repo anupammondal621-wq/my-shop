f95a9217e24ed269144a554fd947d279bbda1f0e
Anupam Mondal <anupammondal621@gmail.com>
2026-04-07 23:28:01 +0200

    your message

--- components/Header.tsx
+++ components/Header.tsx
@@ -46,7 +46,7 @@ export default function Header() {
     <header className="fixed top-0 left-0 right-0 z-50 h-[70px] border-b bg-white text-black">
       <div className="flex h-full items-center justify-between px-4 sm:px-6 lg:px-8">
         <Link href="/" className="shrink-0 text-lg font-bold sm:text-xl">
-          My Shop
+          BongoMithai
         </Link>
 
         <nav className="flex items-center gap-3 whitespace-nowrap text-xs font-medium sm:gap-6 sm:text-sm">
