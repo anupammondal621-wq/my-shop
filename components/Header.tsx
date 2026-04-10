4bc6daf51b274f7cd9294f0466ed10f88e5cb5c7
Anupam Mondal <anupammondal621@gmail.com>
2026-04-10 16:32:11 +0200

    Adjust logo position slightly to the left

--- components/Header.tsx
+++ components/Header.tsx
@@ -198,9 +198,9 @@ export default function Header() {
 {/* LOGO */}
 <Link
   href="/"
-  className="absolute left-1/2 -translate-x-1/2 flex items-center whitespace-nowrap"
+  className="absolute left-1/2 -translate-x-[59%] flex items-center whitespace-nowrap"
 >
-  <div className="flex items-center gap-0.8">
+  <div className="flex items-center gap-0.9">
     <img
       src="/logo_vector.svg"
       alt="Logo"
