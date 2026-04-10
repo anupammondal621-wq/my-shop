6bf2fe035feea9103818ac2a7133d1b074368055
Anupam Mondal <anupammondal621@gmail.com>
2026-04-11 00:12:17 +0200

    your message

--- app/page.tsx
+++ app/page.tsx
@@ -482,7 +482,7 @@ export default function Home() {
               alt="Vogue"
               width={160}
               height={60}
-              className="w-[190px] object-contain"
+              className="w-[200px] object-contain"
             />
           </div>
 
@@ -512,7 +512,7 @@ export default function Home() {
               alt="Nykaa"
               width={140}
               height={60}
-              className="w-[110px] object-contain"
+              className="w-[190px] object-contain"
             />
           </div>
 
