26562664a7f23588456350da2485439dac485b43
Anupam Mondal <anupammondal621@gmail.com>
2026-05-02 15:40:00 +0200

    fix: improve performance, remove delay, optimize header

--- components/AddToCartButton.tsx
+++ components/AddToCartButton.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { addToCart, loadCart, type ProductForCart } from "@/utils/cart";
 
+
 type AddToCartButtonProps = {
   product: ProductForCart;
   fullWidth?: boolean;
